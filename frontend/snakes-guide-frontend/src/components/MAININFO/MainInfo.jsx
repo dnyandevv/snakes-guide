6e738bb40697366a3d866b9d35752593d08b4f9b
Your Name <you@example.com>
2025-12-24 11:57:48 +0530

    DOnw with the responsiveness

--- frontend/snakes-guide-frontend/src/components/MAININFO/MainInfo.jsx
+++ frontend/snakes-guide-frontend/src/components/MAININFO/MainInfo.jsx
@@ -44,53 +44,79 @@ export default function MainInfo() {
     return (
         
         <div className="relative">
-            <header className="bg-gray-500 fixed w-full z-10">
+            <header className="bg-gray-500 fixed w-full z-20">
                 <MainNav />
             </header>
-            <div className="w-full h-[50vh] md:h-[60vh] lg:h-[70vh] overflow-hidden bg-[#1A1A1A]">
+
+
+
+            <div className="w-full h-[40vh] md:h-[60vh] lg:h-[70vh] overflow-hidden bg-[#1A1A1A]">
                 <img 
                     className="w-full h-full object-cover object-center shadow-2xl" 
                     src="/main/common.png"
                     alt="Common Krait"
                 />
             </div>
+
+
+
             <main className="relative ">
-                        <div className="flex flex-col flex-wrap max-w-[90%] mx-auto pt-12 z-10 bg-[#f7f8f2] px-6 pb-20 rounded-t-[2rem] -mt-12 shadow-2xl my-3">
-                            <div className="p-1 bg-gray-100 w-full flex items-center justify-start gap-8 px-4">
-                                <div className="w-[30%]">
-                                    <h1 className="text-3xl">
-                                        Common Krait
-                                    </h1>
-                                    <p className="text-xl text-gray-700">Bungarus caeruleus</p>
+
+                        <div className="flex flex-col max-w-[95%] md:max-w-[90%] mx-auto pt-8 md:pt-12 z-10 bg-[#f7f8f2] px-4 md:px-6 pb-12 md:pb-20 rounded-t-[2rem] -mt-12 shadow-2xl my-3">
+                            
+                            
+                            <div className="bg-gray-100 w-full flex md:flex-row flex-col items-start md:items-center justify-start p-4 gap:4 md:gap-8 p-4 rounded-xl ">
+                                <div className="w-full md:w-[30%]">
+                                    <h1 className="text-2xl md:text-3xl font-bold">Common Krait</h1>
+                                    <p className="text-lg md:text-xl text-gray-700 italic">Bungarus caeruleus</p>
                                 </div>
-                                <div className="flex">
-                                    <h1 className="text-xl rounded-[2rem] bg-yellow-300 justify-center items-center py-2 px-5 m-2">Venomous</h1>
-                                    <h2 className="text-xl rounded-[0.5rem] bg-gray-300 justify-center items-center py-2 px-5 m-2">Indian Subregions</h2>
+                                <div className="flex flex-wrap gap-2">
+                                    <span className="text-sm md:text-xl rounded-full bg-yellow-300 py-1 px-4 font-semibold"
+                                    >
+                                        Venomous
+                                    </span>
+                                    <span className="text-sm md:text-xl rounded-lg bg-gray-300  py-1 px-4"
+                                    >
+                                        Indian Subregions
+                                    </span>
                                 </div>
                             </div>
-                            <div className="flex m-8">
-                                <div className="bg-gray-200/50 rounded-[1rem] m-2 flex-[5]">
+
+
+
+                            <div className="flex flex-col lg:flex-row mt-6 md:m-8 gap-4">
+
+
+                                <div className="bg-gray-200/50 rounded-[1rem] w-full p-2 flex-[5]">
                                     <MapInfo snake_data={COMMON_KRAIT_DATA} />
                                 </div>
+
+
                                 <div className="bg-gray-100/70 rounded-[1rem] m-2 flex-[4]">
-                                    <div className="flex flex-col justify-center items-center gap-4 p-4 m-8 ">
-                                        <img 
-                                            className="w-[80%] justify-center items-center flex-[2] rounded-[1rem] m-10"
-                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6aC0YQxC9s_8XQqGyg6zNejqajO1hDeDRw&s" alt="head" />
-                                        <img 
-                                            className="w-[80%] justify-center object-contain items-center flex-[2] rounded-[1rem] m-10"
-                                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6aC0YQxC9s_8XQqGyg6zNejqajO1hDeDRw&s" alt="b" />
-                                        
-                                        <div className="flex-[1] m-10 flex flex-col justify-center items-center p-4">
-                                            <h1 className="font-bold m-2 self-start">LOOKALIKE</h1>
-                                            <SnakeCard 
-                                                name="Indian Cobra" 
-                                                scientificName="Naja naja" 
-                                                type="Venomous" 
-                                                image="/the-big-four/kc.png"
-                                            />
+                                    <div className="flex flex-col justify-center items-center gap-6 p-6">
+                                        <div className="flex flex-row lg:flex-col gap-4 w-ful justify-center md:items-center">
+                                            <img 
+                                                className="w-1/2 lg:w-[80%] aspect-square object-cover rounded-[1rem] shadow-md"
+                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6aC0YQxC9s_8XQqGyg6zNejqajO1hDeDRw&s" alt="head" />
+                                            <img 
+                                                className="w-1/2 lg:w-[80%] aspect-square object-cover rounded-[1rem] shadow-md"
+                                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt6aC0YQxC9s_8XQqGyg6zNejqajO1hDeDRw&s" alt="b" />
+                                            
+                                        </div>
+
+
+                                        <div className="w-full flex flex-col justify-center items-center">
+                                            <h1 className="font-bold mb-4 self-start border-b-2 border-gray-400 w-full">LOOKALIKE</h1>
+                                            <div className="w-full max-w-[300px]">
+                                                <SnakeCard 
+                                                    name="Indian Cobra" 
+                                                    scientificName="Naja naja" 
+                                                    type="Venomous" 
+                                                    image="/the-big-four/kc.png"
+                                                />
+                                            </div>
                                             <button
-                                                className="bg-gray-500 m-2 rounded-full text-white px-4 py-2 hover:bg-gray-700 transition-colors"
+                                                className="mt-4 bg-gray-600 w-full sm:w-auto rounded-full text-white px-8 py-2 hover:bg-gray-800 transition-colors font-medium"
                                             >
                                                 Compare
                                             </button>
@@ -100,7 +126,7 @@ export default function MainInfo() {
                             </div>                       
                         </div>
 
-                        <footer className="bg-gray-500 text-white text-center py-4 mt-12">
+                        <footer className="bg-gray-500 text-white text-center py-6 mt-12 text-sm">
                             &copy; {new Date().getFullYear()} Saapmahiiite. Made for educational purposes. @Dnyan_dev
                         </footer>
             </main>
