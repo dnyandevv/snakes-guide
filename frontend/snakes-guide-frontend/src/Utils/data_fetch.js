ce9b56e32e5006bb51b45e633522a0568548c217
Your Name <you@example.com>
2026-01-06 14:58:33 +0530

    Prepare for vercel Deployment

--- frontend/snakes-guide-frontend/src/Utils/data_fetch.js
+++ frontend/snakes-guide-frontend/src/Utils/data_fetch.js
@@ -1,7 +1,7 @@
 const BASE_URL = "http://127.0.0.1:8000";
 
 export async function fetchSnakes() {
-  const res = await fetch(`${BASE_URL}/snakes/`);
+  const res = await fetch(`api/snakes/`);
   if (!res.ok) {
     throw new Error("Failed to fetch snakes");
   }
@@ -9,7 +9,7 @@ export async function fetchSnakes() {
 }
 
 export async function fetchSnakeById(id) {
-  const res = await fetch(`${BASE_URL}/snakes/${id}`);
+  const res = await fetch(`api/snakes/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch snake");
   }
